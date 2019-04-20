124dd8b7fca164913f9a6e502d0530ea850d6be0
Reuben <reubendutton@gmail.com>
2019-04-20 18:24:03 +1000

    Fixed mobile functionality for bot list

--- bots/botpages.js
+++ bots/botpages.js
@@ -10,29 +10,23 @@ $(document).ready(function(){
 
         $.getJSON('botpages.json', function(data){
             $.each(data, function(i,page){
-                var botcardlist = ['<div class="container page-card',
+                var botcardlist = ['<div class="container page-card d-none d-lg-block',
                 '<div class="row">',
                 '<div class="col-12 mt-3">',
                 '<div class="card">',
                 '<div class="card-horizontal">',
-                '<div class="img-square-wrapper p-1 m-3 border border-primary rounded-circle d-none d-lg-block">',
+                '<div class="img-square-wrapper p-1 m-3 border border-primary rounded-circle">',
                 '<img class="rounded-circle" src="' + page.dp_link + '" style="height: 80px; width: 80px;" alt="Profile picture">',
                 '</div>',
-                '<div class="card-body text-truncate d-none d-lg-block">',
-                '<h3 class="d-inline card-title font-weight-bold" title="' + page.title + '">' + page.title + '</h3>',
-                '<h4 style="margin-top: .375rem;">',
-                '</h4>',
-                '</div>',
-                '<div class="card-body col-7 d-block d-lg-none">',
+                '<div class="card-body text-truncate">',
                 '<h3 class="d-inline card-title font-weight-bold" title="' + page.title + '">' + page.title + '</h3>',
                 '<h4 style="margin-top: .375rem;">',
                 '</h4>',
                 '</div>',
                 '<div class="card col-auto align-middle" style="margin: 1.225rem; border: none;">',
-                '<div class="card-body d-none d-lg-inline">',
-                '<a class="btn btn-primary d-none d-lg-inline" href="' + page.fb_link + '" role="button" target="_blank">Facebook</a>',
+                '<div class="card-body">',
+                '<a class="btn btn-primary" href="' + page.fb_link + '" role="button" target="_blank">Facebook</a>',
                 '</div>',
-                '<a href="' + page.fb_link + '"><img src="../fb-logo.png" class="d-inline d-lg-none" style="margin-top: 50%; margin-bottom: 50%;" width="50" target="_blank"></a>',
                 '</div>',
                 '</div>',
                 '</div>',
@@ -41,12 +35,32 @@ $(document).ready(function(){
                 '</div>',
                 '</div>',
                 '</div>'];
+                var botcardxslist = ['<div class="card d-block d-lg-none">',
+                 '<div class="card-body">',
+                 '<div class="d-flex flex-items-center flex-justify-center flex-auto">',
+                 '<div class="border border-primary rounded-circle" style="margin: 1.5rem auto 1.5rem auto; max-width: 50%; height: 50%; overflow: hidden;">',
+                 '<img class="card-img-top" style="height: 150px; width: 150px" src="' + page.dp_link + '" alt="Profile pic">',
+                 '</div>',
+                 '</div>',
+                 '<h5 class="card-title">' + page.title + '</h5>',
+                 '<h5 style="margin-top: .125rem;">',
+                 '</h5>',
+                 '<div class="card col-auto align-middle" style="margin: 0.875rem; border: none;">',
+                 '<div class="card-body" style="padding: auto;">',
+                 '<a class="btn btn-primary float-left" href="' + page.fb_link + '" role="button" target="_blank">Facebook</a>',
+                 '</div>',
+                 '</div>',
+                 '</div>',
+                 '</div>'];
+
                 if (page.github === undefined) {
-                    gitbutton = '<a class="btn btn-secondary disabled" href="' + page.github + '" role="button" target="_blank">Github</a>';
-                    botcardlist.splice(21, 0, gitbutton)
+                    gitbutton = '<a class="btn btn-secondary float-right float-md-none disabled" href="' + page.github + '" role="button" target="_blank">Github</a>';
+                    botcardlist.splice(16, 0, gitbutton)
+                    botcardxslist.splice(13, 0, gitbutton)
                 } else {
-                    gitbutton = '<a class="btn btn-primary" href="' + page.github + '" role="button" target="_blank">Github</a>';
-                    botcardlist.splice(21, 0, gitbutton)
+                    gitbutton = '<a class="btn btn-primary float-right float-md-none" href="' + page.github + '" role="button" target="_blank">Github</a>';
+                    botcardlist.splice(16, 0, gitbutton)
+                    botcardxslist.splice(13, 0, gitbutton)
                     botcardlist[0] += " github-tag"
                 }
                 var tags = page.tags;
@@ -54,49 +68,56 @@ $(document).ready(function(){
                     if (tag=="Interactive") {
                         badge = '<span class="badge badge-interactive" style="margin: 0.062rem;">Interactive</span>'
                         botcardlist.splice(11, 0, badge);
+                        botcardxslist.splice(9, 0, badge);
                         botcardlist[0] += " interactive-tag"
                     } else if (tag=="Video") {
                         badge = '<span class="badge badge-video" style="margin: 0.062rem;">Video</span>'
                         botcardlist.splice(11, 0, badge);
+                        botcardxslist.splice(9, 0, badge);
                         botcardlist[0] += " video-tag"
                     } else if (tag=="Image") {
                         badge = '<span class="badge badge-image" style="margin: 0.062rem;">Image</span>'
                         botcardlist.splice(11, 0, badge);
+                        botcardxslist.splice(9, 0, badge);
                         botcardlist[0] += " image-tag"
                     } else if (tag=="Text") {
                         badge = '<span class="badge badge-text" style="margin: 0.062rem;">Text</span>'
                         botcardlist.splice(11, 0, badge);
+                        botcardxslist.splice(9, 0, badge);
                         botcardlist[0] += " text-tag"
                     } else if (tag=="Dead") {
                         badge = '<span class="badge badge-dead" style="margin: 0.062rem;">Dead</span>'
                         botcardlist.splice(11, 0, badge);
+                        botcardxslist.splice(9, 0, badge);
                         botcardlist[0] += " dead-tag"
                     } else if (tag=="Alive") {
                         badge = '<span class="badge badge-alive" style="margin: 0.062rem;">Alive</span>'
                         botcardlist.splice(11, 0, badge);
+                        botcardxslist.splice(9, 0, badge);
                         botcardlist[0] += " alive-tag"
                     };
                 })
                 botcardlist[0] += '">'
                 $('.loading-bots').hide()
                 $('main').append(botcardlist.join("\n"));
-                $('.dead-tag').hide();
+                $('main').append(botcardxslist.join("\n"));
+                $('.dead-tag').attr('style','display:none !important');
             });
         });
     });
 
     $('#tagselect').change(function() {
-        $('.page-card').hide();
+        $('.page-card').attr('style','display:none !important');
         if ($(this).val() != "none") {
-            $('.' + $(this).val()).show();
+            $('.' + $(this).val()).attr('style','');;
         } else {
-            $('.page-card').show();
+            $('.page-card').attr('style','');;
         };
     });
 
     $("#search-button").click(function() {
-      $('.page-card').hide();
-      $('.page-card:Contains(' + $("#bot-search").val() + ')').show();
+      $('.page-card').attr('style','display:none !important');;
+      $('.page-card:Contains(' + $("#bot-search").val() + ')').attr('style','');;
       var filterbtns = ["github", "interactive", "alive", "dead", "video", "image", "text"];
       $.each(filterbtns, function(index, tag){
         $("#" + tag + "-checkbox").prop('checked', false);
@@ -106,7 +127,7 @@ $(document).ready(function(){
 
     $("#clear-button").click(function() {
       $('#bot-search').val("")
-      $('.page-card').show();
+      $('.page-card').attr('style','');;
       var filterbtns = ["github", "interactive", "alive", "dead", "video", "image", "text"];
       $.each(filterbtns, function(index, tag){
         $("#" + tag + "-checkbox").prop('checked', false);
@@ -150,11 +171,11 @@ $(document).ready(function(){
         }
       });
 
-      $(".page-card").hide();
+      $(".page-card").attr('style','display:none !important');;
       if (visible.length > 0) {
-        $(visible.join("") + ':Contains(' + $("#bot-search").val() + ')').show();
+        $(visible.join("") + ':Contains(' + $("#bot-search").val() + ')').attr('style','');;
       } else {
-        $(".page-card").show();
+        $(".page-card").attr('style','');;
       }
     });
 });
